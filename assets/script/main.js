dfc6b1e3c0f8cc40a07e6bcdd4bae6e3373df54f
Ahmedreda20 <62707388+Ahmedreda20@users.noreply.github.com>
2021-07-12 01:10:50 +0200

    first commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -223,9 +223,113 @@ function checkCurrentHrefWithCurrentURI(url) {
 // hide preload after page load
 
 function HidePreload(elem) {
-  elem.classList.replace("flex", "hidden");
+  let opacityValue = 100;
+  let counter = setInterval(() => {
+    opacityValue--;
+    elem.style.opacity = `${opacityValue}%`;
+    if (opacityValue === 0) {
+      clearInterval(counter);
+      elem.removeAttribute("style");
+      elem.classList.replace("flex", "hidden");
+      elem.querySelectorAll("svg path").forEach((path) => {
+        path.style.cssText = "animation-play-state: paused;";
+      });
+      return false;
+    }
+  }, 15);
 }
 
 window.onload = () => {
   HidePreload(document.querySelector(".preload__container"));
 };
+
+// portfolio filter
+const btnFilters = document.querySelectorAll(".btn__portfolio--filter");
+const portfolioItemsBox = document.querySelectorAll(".portfolio__item");
+
+handleSlidersOnClick(portfolioItemsBox);
+
+btnFilters.forEach((btnFilter) => {
+  let CurrentPortfolioItems = [];
+  btnFilter.addEventListener("click", () => {
+    let btnFilterType = btnFilter.dataset.portfolioType;
+    btnFilters.forEach((btnF) => {
+      btnF.classList.remove("btn__portfolio--active");
+    });
+    btnFilter.classList.add("btn__portfolio--active");
+    portfolioItemsBox.forEach((portfolioItem) => {
+      let portfolioType = portfolioItem.dataset.portfolioType;
+      if (btnFilterType === portfolioType) {
+        portfolioItem.classList.remove("hidden");
+      } else if (btnFilterType === "all") {
+        portfolioItem.classList.remove("hidden");
+      } else {
+        portfolioItem.classList.add("hidden");
+      }
+
+      if (!portfolioItem.classList.contains("hidden")) {
+        CurrentPortfolioItems.push(portfolioItem);
+        handleSlidersOnClick(CurrentPortfolioItems);
+      }
+    });
+  });
+});
+
+// portfolio gallery viewer
+var portfolioViewer = document.querySelector(".portfolio__viewer");
+// close portfolio viewer when click on close button
+portfolioViewer.querySelector(".portfolio__btn--close").onclick = () => {
+  portfolioViewer.classList.add("hidden");
+};
+
+function handleSlidersOnClick(itemsArray) {
+  let portfolioCurrentItems = itemsArray;
+  let numbers = document.querySelectorAll(".portfolio__numbers--box span");
+  portfolioCurrentItems.forEach((portfolio, portfolioCurrentIndex) => {
+    portfolio.addEventListener("click", () => {
+      numbers[0].innerHTML = portfolioCurrentIndex + 1;
+      numbers[1].innerHTML = portfolioCurrentItems.length;
+      portfolioViewer.classList.remove("hidden");
+      document.querySelector(".portfolio__viewer--img").src =
+        portfolio.querySelector("img").src;
+      sliderButtonsControl(
+        ".portfolio__btn--next",
+        ".portfolio__btn--prev",
+        portfolioCurrentIndex,
+        portfolioCurrentItems,
+        numbers
+      );
+    });
+  });
+}
+
+function sliderButtonsControl(next, prev, items, portfolioItems, numbers) {
+  const btnNext = document.querySelector(next);
+  const btnPrev = document.querySelector(prev);
+  let sliders = items;
+  document.querySelector(".portfolio__viewer--img").src =
+    portfolioItems[sliders].querySelector("img").src;
+  btnNext.onclick = () => {
+    if (sliders < portfolioItems.length - 1) {
+      sliders++;
+      portfolioViewer.querySelector("img").src =
+        portfolioItems[sliders].querySelector("img").src;
+      showCurrentIndex(sliders, numbers, true);
+    }
+  };
+
+  btnPrev.onclick = () => {
+    if (sliders > 0) {
+      sliders--;
+      portfolioViewer.querySelector("img").src =
+        portfolioItems[sliders].querySelector("img").src;
+      showCurrentIndex(sliders, numbers, false);
+    }
+  };
+}
+
+function showCurrentIndex(slideIndex, numbers, action) {
+  action
+    ? (numbers[0].innerHTML = slideIndex + 1)
+    : (numbers[0].innerHTML = slideIndex + 1);
+}
